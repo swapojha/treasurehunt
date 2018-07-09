7662324cf067c8fbabe33da1eb566a460fb81e3f
Swapnil Ojha <swapnilojha@outlook.com>
2018-07-09 23:00:00 +0530

    Sql database Config

--- TreasureHunt/settings.py
+++ TreasureHunt/settings.py
@@ -91,9 +91,16 @@ LOGIN_REDIRECT_URL = 'home'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'linpaws$TreasureHunt',
+        'USER': 'linpaws',
+        'PASSWORD': 'eft78LUI&+@',
+        'HOST': 'linpaws.mysql.pythonanywhere-services.com',
     }
 }
 
