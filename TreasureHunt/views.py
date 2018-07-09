d563d81a63638a7212b7e362f571ba8514fe7291
Swapnil Ojha <swapnilojha@outlook.com>
2018-07-10 00:46:04 +0530

    Added Leaderboard

--- TreasureHunt/views.py
+++ TreasureHunt/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.contrib.auth import logout
-from hunt.models import LoggedInUser
+from hunt.models import LoggedInUser, GameUser
 from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.sessions.models import Session
 from django.contrib import messages
@@ -18,20 +18,22 @@ def rules(request):
     return HttpResponse('Rules page')
 
 def leaderboard(request):
-    return HttpResponse('Leaderboard page')
+    # No need for user authentication
+    game_users = GameUser.objects.filter(user__is_staff=True).order_by('score')
+    return render(request,'leaderboard.html',{'gameusers',game_users})
 
 def homepage_view(request):
     if request.user.is_authenticated:
         msg = 'You are logged in!'
-        print("Back here")
+        #print("Back here")
         LoggedInUser.objects.get_or_create(user = request.user)
         stored_session_key = request.user.logged_in_user.session_key
         # if there is a stored_session_key  in our database and it is
         # different from the current session, delete the stored_session_key
         # session_key with from the Session table
-        print(stored_session_key)
+        #print(stored_session_key)
         if stored_session_key and stored_session_key != request.session.session_key:
-            print("Found Previous login")
+            # print("Found Previous login")
             Session.objects.get(session_key=stored_session_key).delete()
         request.user.logged_in_user.session_key = request.session.session_key
         request.user.logged_in_user.save()
