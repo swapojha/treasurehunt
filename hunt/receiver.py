e91111153f32ccf11ef57dd76781ca91a04adfe1
Swapnil Ojha <swapnilojha@outlook.com>
2018-07-09 16:30:11 +0530

    Player Profile debug

--- hunt/receiver.py
+++ hunt/receiver.py
@@ -3,6 +3,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.utils import timezone 
 from .models import GameUser, Question, GameUserData
+from django.core.exceptions import ObjectDoesNotExist
 
 
 @receiver(post_save, sender=User)
@@ -10,5 +11,8 @@ def make_player_profile(sender, instance, created, **kwargs):
       if created:
           game_user = GameUser.objects.create(user=instance)
           level = 1
-          quest = Question.objects.get(level=level)
-          GameUserData.objects.create(game_user = game_user, question = quest,start_time = timezone.now(),score = quest.score)
+          try:
+            quest = Question.objects.get(level=level)
+            GameUserData.objects.create(game_user = game_user, question = quest,start_time = timezone.now(),score = quest.score)
+          except ObjectDoesNotExist:
+              pass 
