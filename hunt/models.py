29380e0e75e6298b151fcc33f06168fc0cb11496
Swapnil Ojha <swapnilojha@outlook.com>
2018-07-28 05:09:19 +0530

    Added Timeout

--- hunt/models.py
+++ hunt/models.py
@@ -19,8 +19,8 @@ class GameUser(models.Model):
     score = models.IntegerField(default=0)
     timestamp = models.DateTimeField(null=True)
     total_attempts = models.IntegerField(default=0)
-    last_attempt = models.DateTimeField(default=timezone.now())
-    timeout_attempts = models.IntegerField(0)
+    last_attempt = models.DateTimeField(null=True)
+    timeout_attempts = models.IntegerField(default=0)
     def levelup(self,ques_score,timestamp,attempts):
         self.level+=1
         self.score+=ques_score
