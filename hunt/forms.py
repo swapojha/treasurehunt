3a4a7cdd29a1d9c1626115c164e54d7a5e69d260
Swapnil Ojha <swapnilojha@outlook.com>
2018-08-15 16:06:54 +0530

    Ajax Response Implementation

--- hunt/forms.py
+++ hunt/forms.py
@@ -2,4 +2,4 @@ from django import forms
 from .models import Question
 
 class answer_form(forms.Form):
-    answer = forms.CharField(widget=forms.TextInput(attrs=dict(required=True, id="answer",max_length=100,placeholder="Your answer: ")))
+    answer = forms.CharField(widget=forms.TextInput(attrs=dict(required=True,max_length=100,placeholder="Your answer: ")))
