e91645b3a56810405d038e108d58f21aef27c420
Swapnil Ojha <swapnilojha@outlook.com>
2018-08-16 16:12:10 +0000

    M

--- TreasureHunt/settings.py
+++ TreasureHunt/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 USE_TZ = True
+SECURE_SSL_REDIRECT = True
 TIME_ZONE = 'Asia/Kolkata'
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
